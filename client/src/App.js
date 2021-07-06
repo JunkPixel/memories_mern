05b33a7647f6f6a379ef405ba3f6dd14fce3fcec
JunkPixel <ricardotomaz027@gmail.com>
2021-07-06 19:16:10 +0100

    basic structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,12 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
-import { useDispatch } from "react-redux";
 
 import Posts from "./components/Posts/Posts";
 import Form from "./components/Form/Form";
-import { getPosts } from "./actions/posts";
-import useStyles from "./styles";
+
 import memories from "./images/memories.png";
 
 const App = () => {
-  const [currentId, setCurrentId] = useState(0);
-  const dispatch = useDispatch();
-  const classes = useStyles();
-
-  useEffect(() => {
-    dispatch(getPosts());
-  }, [currentId, dispatch]);
-
   return (
     <Container maxWidth="lg">
       <AppBar position="static" color="inherit">
@@ -29,7 +19,7 @@ const App = () => {
         <Container>
           <Grid
             container
-            justify="space-between"
+            justifyContent="space-between"
             alignItems="stretch"
             spacing={3}
           >
